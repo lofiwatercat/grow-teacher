f3948d033b619b7f09f1e92a502a2e18d669b8e3
deli123 <liderek~@gmail.com>
2022-09-19 16:34:13 -0700

    add middle text on splash page

--- frontend/src/views/SplashPage/index.js
+++ frontend/src/views/SplashPage/index.js
@@ -6,6 +6,14 @@ const SplashPage = () => {
     <>
       <div className="splash-background">
         <img id="splash-image" src={splashImage} />
+        <div className="splash-middle-text">
+          <div>
+            <h1>Text here</h1>
+          </div>
+          <div>
+            <button>Start for free</button>
+          </div>
+        </div>
       </div>
     </>
   );
