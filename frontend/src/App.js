e3e4be6be406ee52671420e0696242a2db724a95
deli123 <liderek~@gmail.com>
2022-09-19 11:37:13 -0700

    setup frontend with dependencies and login form

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+
+import { AuthRoute, ProtectedRoute } from "./components/Routes/Routes";
+
+import LoginForm from "./components/SessionForms/LoginForm";
+import SignupForm from "./components/SessionForms/SignupForm";
+
+import { getCurrentUser } from "./store/reducers/session_reducer";
+
+function App() {
+  const [loaded, setLoaded] = useState(true);
+  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //   dispatch(getCurrentUser()).then(() => setLoaded(true));
+  // }, [dispatch]);
+
+  return (
+    loaded && (
+      <>
+        <Switch>
+          {/* <AuthRoute exact path="/" /> */}
+          <Route exact path="/login" component={LoginForm} />
+          {/* <Route exact path="/signup" component={SignupForm} /> */}
+        </Switch>
+      </>
+    )
+  );
+}
+
+export default App;
