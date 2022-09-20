198b4ca26307a027adbc118c9d5f820603350154
deli123 <liderek63@gmail.com>
2022-09-20 14:53:10 -0700

    create scalable items list

--- frontend/src/components/PostsForm/index.js
+++ frontend/src/components/PostsForm/index.js
@@ -5,14 +5,10 @@ import "./PostsForm.css";
 
 const PostsForm = () => {
   const sessionUser = useSelector((state) => state.session.user);
+  const [itemFields, setItemFields] = useState([
+    { name: "", totalCost: "", amount: "", comments: "" },
+  ]);
   const dispatch = useDispatch();
-  let item = {
-    name: "",
-    totalCost: "",
-    amount: "",
-    comments: "",
-  };
-  const [newItem, setNewItem] = useState(item);
 
   let post = {
     title: "",
@@ -21,69 +17,96 @@ const PostsForm = () => {
   };
   const [newPost, setNewPost] = useState(post);
 
-  const handleItemChange = (e, field) => {
-    setNewItem({ ...newItem, [field]: e.currentTarget.value });
+  const handleItemChange = (e, index) => {
+    let data = [...itemFields];
+    data[index][e.target.name] = e.target.value;
+    setItemFields(data);
+  };
+
+  const addItems = () => {
+    let newItem = { name: "", totalCost: "", amount: "", comments: "" };
+    setItemFields([...itemFields, newItem]);
+  };
+
+  const removeItem = (index) => {
+    let data = [...itemFields];
+    data.splice(index, 1);
+    setItemFields(data);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    let itemInput = [newItem]
 
     setNewPost({
       ...newPost,
-      items: itemInput,
+      items: itemFields,
     });
 
     // items field coming in as empty array on submit even though itemInput is not empty
     // thus, have to make a copy of newPost, and reassign the items field
     let copy = newPost;
-    copy.items = itemInput;
+    copy.items = itemFields;
+    console.log(copy);
     dispatch(createPost(copy));
   };
 
   return (
     <>
-    {sessionUser && 
-      <div className="posts-form-container">
-        <form className="posts-form">
-          <input
-            placeholder="Title"
-            onChange={(e) => setNewPost({ ...newPost, title: e.target.value })}
-            required
-          />
-          <input
-            placeholder="Body"
-            onChange={(e) => setNewPost({ ...newPost, body: e.target.value })}
-            required
-          />
-          <div>
-            <label>
-              item
-              <input
-                placeholder="Name"
-                onChange={(e) => handleItemChange(e, "name")}
-                required
-              />
-              <input
-                placeholder="Total Cost"
-                onChange={(e) => handleItemChange(e, "totalCost")}
-                required
-              />
-              <input
-                placeholder="Amount"
-                onChange={(e) => handleItemChange(e, "amount")}
-                required
-              />
-              <input
-                placeholder="Comments"
-                onChange={(e) => handleItemChange(e, "comments")}
-              />
-            </label>
-          </div>
-          <button onClick={handleSubmit}>Create</button>
-        </form>
-      </div>
-    }
+      {sessionUser && (
+        <div className="posts-form-container">
+          <form className="posts-form">
+            <input
+              placeholder="Title"
+              onChange={(e) =>
+                setNewPost({ ...newPost, title: e.target.value })
+              }
+              required
+            />
+            <input
+              placeholder="Body"
+              onChange={(e) => setNewPost({ ...newPost, body: e.target.value })}
+              required
+            />
+            {itemFields.map((input, index) => {
+              return (
+                <div key={index}>
+                  item
+                  <input
+                    name="name"
+                    placeholder="Name"
+                    onChange={(e) => handleItemChange(e, index)}
+                    value={input.name}
+                    required
+                  />
+                  <input
+                    name="totalCost"
+                    placeholder="Total Cost"
+                    onChange={(e) => handleItemChange(e, index)}
+                    value={input.name}
+                    required
+                  />
+                  <input
+                    name="amount"
+                    placeholder="Amount"
+                    onChange={(e) => handleItemChange(e, index)}
+                    value={input.name}
+                    required
+                  />
+                  <input
+                    name="comments"
+                    placeholder="Comments"
+                    onChange={(e) => handleItemChange(e, index)}
+                    value={input.name}
+                  />
+                  <button onClick={() => removeItem(index)}>Remove</button>
+                </div>
+              );
+            })}
+            <button onClick={addItems}>Add item</button>
+            <button onClick={handleSubmit}>Create</button>
+          </form>
+        </div>
+      )}
     </>
   );
 };
