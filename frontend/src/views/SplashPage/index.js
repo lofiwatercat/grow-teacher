55a6839bc3ea4b2d3c5e0ac1cddc6721a2b79449
deli123 <liderek63@gmail.com>
2022-09-20 10:35:17 -0700

    start splash page bottom

--- frontend/src/views/SplashPage/index.js
+++ frontend/src/views/SplashPage/index.js
@@ -1,18 +1,54 @@
 import splashImage from "../../assets/images/splash-bg.jpg";
 import "./SplashPage.scss";
-import NavBar from "../../components/NavBar"
 
 const SplashPage = () => {
   return (
     <>
       <div className="splash-background">
-        <img id="splash-image" src={splashImage} />
+        <img id="splash-image" src={splashImage} alt="background" />
         <div className="splash-middle-text">
           <div>
             <h1>Text here</h1>
           </div>
           <div>
             <button>Start for free</button>
+          </div>
+        </div>
+      </div>
+      <div className="splash-about">
+        <div className="splash-about-title">
+          <h1>Title</h1>
+        </div>
+        <div className="instructions-container">
+          <div className="instruction">
+            <div className="instruction-item">
+              <div className="instruction-number">
+                <h1>1</h1>
+              </div>
+              <h3>Title</h3>
+              <p>Create a post with a funding goal and equipment checklist.</p>
+            </div>
+          </div>
+          <div className="instruction">
+            <div className="instruction-item">
+              <div className="instruction-number">
+                <h1>2</h1>
+              </div>
+              <h3>Title</h3>
+              <p>
+                Help out teachers in need by contributing item(s)and checking
+                that item off their list.
+              </p>
+            </div>
+          </div>
+          <div className="instruction">
+            <div className="instruction-item">
+              <div className="instruction-number">
+                <h1>3</h1>
+              </div>
+              <h3>Title</h3>
+              <p>Share the love</p>
+            </div>
           </div>
         </div>
       </div>
