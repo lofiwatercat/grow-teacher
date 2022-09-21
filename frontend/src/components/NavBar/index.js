c88881f89f92a4cfd22da7cc4bd2ffc81d8c4142
Derek <liderek63@gmail.com>
2022-09-20 23:02:00 -0700

    style sticky navbar

--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import "./NavBar.scss";
 import { logout } from "../../store/reducers/session_reducer";
+import { useEffect } from "react";
 
 function NavBar() {
   const loggedIn = useSelector((state) => !!state.session.user);
@@ -12,31 +13,52 @@ function NavBar() {
     dispatch(logout());
   };
 
+  window.addEventListener("scroll", () => {
+    const header = document.querySelector(".navbar");
+    header.classList.toggle("sticky", window.scrollY > 0);
+  });
+
   const getLinks = () => {
     if (loggedIn) {
       return (
-        <div className="links-nav">
-          <Link to={"/posts"}>All Posts</Link>
-          <Link to={"/profile"}>Profile</Link>
-          <Link to={"/posts/new"}>Write a Post</Link>
-          <button onClick={logoutUser}>Logout</button>
-        </div>
+        <ul className="links-nav">
+          <li>
+            <Link to={"/posts"}>All Posts</Link>
+          </li>
+          <li>
+            <Link to={"/profile"}>Profile</Link>
+          </li>
+          <li>
+            <Link to={"/posts/new"}>Write a Post</Link>
+          </li>
+          <li>
+            <button onClick={logoutUser}>Logout</button>
+          </li>
+        </ul>
       );
     } else {
       return (
-        <div className="links-auth">
-          <Link to={"/login"}>Login/Signup</Link>
-        </div>
+        <ul className="links-auth">
+          <li>
+            <Link to={"/login"}>Login/Signup</Link>
+          </li>
+        </ul>
       );
     }
   };
 
   return (
     <>
-      <div className="NavBar">
-        <Link id="home-link" to={'/'}>Home</Link>
+      <header className="navbar">
+        <Link to="#" className="logo">
+          Logo
+        </Link>
+        <Link id="home-link" to={"/"}>
+          Home
+        </Link>
         {getLinks()}
-      </div>
+      </header>
+      <section className="banner"></section>
     </>
   );
 }
