9993d2a1e533bb2e49a81b5aa923d8b467c356f4
zehour <zehourchaoui@gmail.com>
2022-09-21 16:46:11 -0700

    fix keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,12 +1,8 @@
-module.exports = {
-    secretOrKey: process.env.SECRET_OR_KEY,
-    mongoURI: process.env.MONGO_URI,
-    isProduction: process.env.NODE_ENV === 'production'
-};
+isProduction: process.env.NODE_ENV === 'production'
 
-
-/*
-S3_ACCESS_KEY: AKIAZM7IALKZR4YGCZ6N
-S3_SECRET_ACESS_KEY: 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
-S3_BUCKET_REGION: us-west-1
-*/
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
+}
+ 
