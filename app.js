1f2f5905f4fd8ce3721c66dcc554286716886add
alan <alan001tk@gmail.com>
2022-09-22 11:18:14 -0700

    Move isProduction in app above where we define routes

--- app.js
+++ app.js
@@ -34,6 +34,29 @@ if (!isProduction) {
     // (In production, React files will be served statically on the Express server)
     app.use(cors());
 }
+
+// Production, deploying to heroku
+if (isProduction) {
+  const path = require('path');
+  // Serve the frontend's index.html file at the root route
+  app.get('/', (req, res) => {
+    res.cookie('CSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, './frontend', 'build', 'index.html')
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  app.use(express.static(path.resolve("./frontend/build")));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  app.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('CSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, './frontend', 'build', 'index.html')
+    );
+  });
+}
   
 app.use(
     csurf({
@@ -76,28 +99,7 @@ app.use((req, res, next) => {
     })
   });
 
-// Production, deploying to heroku
-if (isProduction) {
-  const path = require('path');
-  // Serve the frontend's index.html file at the root route
-  app.get('/', (req, res) => {
-    res.cookie('CSRF-TOKEN', req.csrfToken());
-    res.sendFile(
-      path.resolve(__dirname, './frontend', 'build', 'index.html')
-    );
-  });
 
-  // Serve the static assets in the frontend's build folder
-  app.use(express.static(path.resolve("./frontend/build")));
-
-  // Serve the frontend's index.html file at all other routes NOT starting with /api
-  app.get(/^(?!\/?api).*/, (req, res) => {
-    res.cookie('CSRF-TOKEN', req.csrfToken());
-    res.sendFile(
-      path.resolve(__dirname, './frontend', 'build', 'index.html')
-    );
-  });
-}
 
 
 module.exports = app;
