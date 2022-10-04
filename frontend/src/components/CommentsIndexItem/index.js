d780d5cc21e429031cc52af39321051400978712
Derek <liderek63@gmail.com>
2022-10-04 15:48:06 -0700

    add styling to comments

--- frontend/src/components/CommentsIndexItem/index.js
+++ frontend/src/components/CommentsIndexItem/index.js
@@ -6,6 +6,10 @@ import {
 } from "../../store/reducers/comments_reducer";
 import { useParams } from "react-router-dom";
 import { Pane, Dialog } from "evergreen-ui";
+import { TextareaField } from "evergreen-ui";
+import { CgProfile } from "react-icons/cg";
+import { FiEdit } from "react-icons/fi";
+import { RiDeleteBin6Line } from "react-icons/ri";
 import "./CommentsIndexItem.scss";
 
 const CommentsIndexItem = ({ comment }) => {
@@ -44,14 +48,28 @@ const CommentsIndexItem = ({ comment }) => {
   return (
     <>
       <div className="comments-index-item">
-        <p>{comment.body}</p>
-        <div>
-          {sessionUser && sessionUser._id === comment.author && (
-            <>
-              <button onClick={() => setShowEdit(true)}>Edit</button>
-              <button onClick={() => setShowDelete(true)}>Delete</button>
-            </>
-          )}
+        <div className="comments-item-header">
+          <div className="comments-item-pic">
+            <CgProfile width={"100%"} fontSize={"2.5rem"} />
+          </div>
+          <div className="comments-author">
+            <p>username</p>
+            <div className="comments-item-buttons">
+              {sessionUser && sessionUser._id === comment.author && (
+                <>
+                  <button onClick={() => setShowEdit(true)}>
+                    <FiEdit fontSize={"1.2rem"} />
+                  </button>
+                  <button onClick={() => setShowDelete(true)}>
+                    <RiDeleteBin6Line fontSize={"1.2rem"} />
+                  </button>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="comments-item-body">
+          <p>{comment.body}</p>
         </div>
       </div>
       {(showEdit || showDelete) && (
@@ -67,9 +85,18 @@ const CommentsIndexItem = ({ comment }) => {
           >
             {showEdit ? (
               <div className="comment-modal">
-                <form onSubmit={handleSubmit}>
-                  <input type="text" value={body} onChange={handleChange} />
-                </form>
+                <TextareaField
+                  className="comment-edit-form"
+                  validationMessage={
+                    body.length <= 1
+                      ? "Body must be at least 2 characters"
+                      : null
+                  }
+                  label="Please type your new comment"
+                  onChange={handleChange}
+                  value={body}
+                  required
+                />
               </div>
             ) : (
               <div className="comment-modal">
