e13ba7ea7ddc0fc50ad1a2a0323961d05cc33f98
Derek <liderek63@gmail.com>
2022-10-04 20:02:20 -0700

    add route redirect on login

--- frontend/src/views/PostsFormPage/index.js
+++ frontend/src/views/PostsFormPage/index.js
@@ -1,6 +1,12 @@
 import PostsForm from "../../components/PostsForm";
+import { useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
 
 const PostsFormPage = () => {
+  const sessionUser = useSelector((state) => !!state.session.user);
+
+  if (!sessionUser) return <Redirect to="/login" />;
+  
   return (
     <>
       <PostsForm />
