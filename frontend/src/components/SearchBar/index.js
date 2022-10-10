35ae76da696c9b8c7d25f97c80a2ef8c58df1828
Derek <liderek63@gmail.com>
2022-10-09 22:19:45 -0700

    fix search button autofocus

--- frontend/src/components/SearchBar/index.js
+++ frontend/src/components/SearchBar/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 import "./SearchBar.scss";
@@ -13,6 +13,31 @@ const SearchBar = () => {
     setQuery("");
   };
 
+  useEffect(() => {
+    const search = document.querySelector("#search-input");
+    search.addEventListener("mouseleave", () => {
+      if (search.value) {
+        search.style.width = "350px";
+        search.style.paddingLeft = "10px";
+      } else {
+        search.style.width = "0px";
+        search.style.paddingLeft = "0px";
+      }
+    });
+
+    const searchButton = document.querySelector(".search-button");
+    searchButton.addEventListener("mouseover", () => {
+      search.style.width = "350px";
+      search.style.paddingLeft = "10px";
+    });
+    searchButton.addEventListener("mouseleave", () => {
+      if (!search.value) {
+        search.style.width = "0px";
+        search.style.paddingLeft = "0px";
+      }
+    });
+  }, []);
+
   return (
     <>
       <div className="search-bar-container">
@@ -23,8 +48,9 @@ const SearchBar = () => {
             onChange={(e) => setQuery(e.target.value)}
             placeholder={`Search by title of posts!`}
             autoFocus="autofocus"
+            value={query}
           />
-          <a href="#" onClick={handleSubmit}>
+          <a className="search-button" href="#" onClick={handleSubmit}>
             <FaSearch />
           </a>
         </form>
