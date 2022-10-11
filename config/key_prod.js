0b33375f335297aefe2df559d2253a520099c739
alan <alan001tk@gmail.com>
2022-10-11 11:51:49 -0700

    Add aws keys to prod keys

--- config/key_prod.js
+++ config/key_prod.js
@@ -1,5 +1,12 @@
 module.exports = {
     secretOrKey: process.env.SECRET_OR_KEY,
     mongoURI: process.env.MONGO_URI,
-    isProduction: process.env.NODE_ENV === 'production'
+    isProduction: process.env.NODE_ENV === 'production',
+    
+    s3AccessKey: process.env.S3_ACCESS_KEY,
+    s3SecretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    s3Bucketregion: process.env.S3_BUCKET_REGION,
+    bucketName: process.env.AWS_BUCKET_NAME,
+    uploadedUrl: process.env.AWS_Uploaded_File_URL_LI,
+    
 }
